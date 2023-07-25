6158632241815b67f4ac46f85cbbb3d5ccb993d6
Kevin J. Monter Zea <kjmz.15987@gmail.com>
2023-07-25 17:34:40 -0500

    individual product screen

--- actions/get-products.tsx
+++ actions/get-products.tsx
@@ -21,7 +21,7 @@ const getCategory = async (query: Query): Promise<Product[]> => {
       isFeatured: query.isFeatured,
     }
   })
-  const res = await fetch(URL);
+  const res = await fetch(url);
 
   return res.json();
   
